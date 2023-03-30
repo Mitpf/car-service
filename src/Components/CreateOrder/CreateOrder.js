3427730dc38f2e8deff3cd2f6fac974a6885bcc5
Mitpf <mitfreex@gmail.com>
2023-03-31 01:59:33 +0300

    createOrder component added, gahti gadniq CSS za Form :D

--- src/Components/CreateOrder/CreateOrder.js
+++ src/Components/CreateOrder/CreateOrder.js
@@ -1,61 +1,162 @@
+
+
+import { useState, Fragment } from 'react';
+
+
 import styles from './CreateOrder.module.css';
 
 export const CreateOrder = () => {
 
+    const [countInputs, setCountInputs] = useState([]);
 
+    const addInputfields = (e) => {
+        e.preventDefault();
+        setCountInputs(state => [...state, state.length + 1])
+    }
+    const removeInputfields = (e) => {
+        e.preventDefault();
+        setCountInputs(state => state.slice(0, -1))
+    }
+
+
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const dataF= new FormData(e.target);
+        console.log(Object.fromEntries(dataF));
+    }
     return (
 
         <div className={styles.container}>
             <div className={styles.title}>
-                <h2>Product Order Form</h2>
+                <h2 className={styles.header}>New Order car service</h2>
             </div>
             <div className={styles["d-flex"]}>
-                <form action method>
-                    <label>
-                        <span className={styles.fname}>First Name <span className={styles.required}>*</span></span>
-                        <input type="text" name="fname" />
+
+
+                <form className={styles.form} method="Post" onSubmit={onSubmit}>
+
+                    <label className={styles.label}>
+                        <span className={styles.labelspan}>
+                            type Order
+                            <span className={styles.required}>*</span>
+                        </span>
+
+                        <label>
+                            problem
+                            <input className={styles.inputCheckbox} type="checkbox" name="problem" />
+                        </label>
+
+
+                        <label className={styles.consumables}>
+                            consumables
+                            <input className={styles.inputCheckbox} type="checkbox" name="consumables" />
+                        </label>
+
+
                     </label>
-                    <label>
-                        <span className={styles.lname}>Last Name <span className={styles.required}>*</span></span>
-                        <input type="text" name="lname" />
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            description Title
+                            <span className={styles.required}>*</span>
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>Company Name (Optional)</span>
-                        <input type="text" name="cn" />
+
+                    <label className={styles.label}>
+                        <span className={styles.description}>
+                            Description text
+                            <span className={styles.required}>*</span>
+                        </span>
+                        <textarea className={styles.descriptionText} name="description" />
+
                     </label>
-                   
-                    <label>
-                        <span>Street Address <span className={styles.required}>*</span></span>
-                        <input type="text" name="houseadd" placeholder="House number and street name" required />
+
+
+
+                    <button className={styles.buttonLinks} onClick={addInputfields}> add image links</button>
+                    <button className={styles.buttonLinks} onClick={removeInputfields}> remove image links</button>
+                    {countInputs.map(x => (
+                        <Fragment key={x} >
+
+                            <span className={styles.labelspan}>
+                                Photo {x}
+                            </span>
+                            <input className={styles.inpTxtMailTel} name={`photoLink ${x}`} />
+
+                        </Fragment>
+                    ))}
+
+                    <h4>car Info:</h4>
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            brandModel:
+                            <span className={styles.required}>*</span>
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>&nbsp;</span>
-                        <input type="text" name="apartment" placeholder="Apartment, suite, unit etc. (optional)" />
+
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            product Date
+                            <span className={styles.required}>*</span>
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>Town / City <span className={styles.required}>*</span></span>
-                        <input type="text" name="city" />
+
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            type engine
+                            <span className={styles.required}>*</span>
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>State / County <span className={styles.required}>*</span></span>
-                        <input type="text" name="city" />
+
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            km
+
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>Postcode / ZIP <span className={styles.required}>*</span></span>
-                        <input type="text" name="city" />
+
+
+                    <label className={`${styles.titleInput} ${styles.label}`}>
+                        <span className={styles.labelspan}>
+                            image Link
+
+                        </span>
+
+                        <input className={styles.inpTxtMailTel} name="title" />
+
                     </label>
-                    <label>
-                        <span>Phone <span className={styles.required}>*</span></span>
-                        <input type="tel" name="city" />
-                    </label>
-                    <label>
-                        <span>Email Address <span className={styles.required}>*</span></span>
-                        <input type="email" name="city" />
-                    </label>
+
+
+
+
+
+
+                    <input type="submit" />
+
                 </form>
-               
+
             </div>
-        </div>
+        </div >
 
     );
 }
